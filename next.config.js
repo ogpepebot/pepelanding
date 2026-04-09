d44526236afc40bf7d89c16ea91e0fbc42cc757d
ogpepebot <ogpepebot@proton.me>
2026-04-09 22:59:57 +0000

    Fix config to work without basePath for root deployment

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
+const basePath = process.env.PAGES_BASE_PATH || "";
+
 const nextConfig = {
   output: "export",
   reactStrictMode: true,
+  basePath,
+  assetPrefix: basePath,
   images: { unoptimized: true },
-  basePath: "/pepelanding",
+  env: { NEXT_PUBLIC_BASE_PATH: basePath },
 };
 
 module.exports = nextConfig;
